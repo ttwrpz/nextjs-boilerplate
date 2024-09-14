970e59b64214ea4c87bb3d288bc2a2f3050db3d1
ttwrpz <smrmakeyt@gmail.com>
2024-09-14 14:18:45 +0700

    Initial Commit

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [
+    require("tailwindcss-animate"),
+    require('tailwindcss-text-fill-stroke'),
+    require('@tailwindcss/container-queries'),
+    require('@tailwindcss/forms')({
+      strategy: 'class',
+    }),
+  ],
+};
+export default config;
